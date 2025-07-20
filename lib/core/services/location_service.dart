b5865a8529252b46b9ada8d3cfc349b6937c0fb4
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2025-07-20 22:46:25 +0300

    Create location_service.dart

---
+++ lib/core/services/location_service.dart
@@ -0,0 +1,44 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:logger/logger.dart';
+
+enum MyLocationPermission { allowed, denied, deniedForever }
+
+abstract class LocationService {
+  Future<MyLocationPermission> checkLocationPermission();
+  Future<Position?> getCurrentLocation();
+}
+
+class LocationServiceImp implements LocationService {
+  @override
+  Future<MyLocationPermission> checkLocationPermission() async {
+    bool serviceEnabled = await Permission.location.request().isGranted;
+    if (!serviceEnabled) {
+      return MyLocationPermission.denied;
+    }
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return MyLocationPermission.denied;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return MyLocationPermission.deniedForever;
+    }
+    return MyLocationPermission.allowed;
+  }
+
+  @override
+  Future<Position?> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        locationSettings: LocationSettings(accuracy: LocationAccuracy.high),
+      );
+      return position;
+    } catch (e) {
+      Logger().i("Error getting location: $e");
+    }
+    return null;
+  }
+}
