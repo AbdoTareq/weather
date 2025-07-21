1e2f950daeeccb760e38e33d5deef2919fe022c3
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2025-07-21 06:55:37 +0300

    feat: add weather injection

--- lib/core/sl/injection_container.dart
+++ lib/core/sl/injection_container.dart
@@ -3,6 +3,11 @@ import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:requests_inspector/requests_inspector.dart';
 import 'package:weather/core/features/location/presentation/cubit/location_cubit.dart';
+import 'package:weather/core/features/weather/data/datasource/weather_remote_data_source.dart';
+import 'package:weather/core/features/weather/data/repo/weather_repo_imp.dart';
+import 'package:weather/core/features/weather/domain/repo/weather_repo.dart';
+import 'package:weather/core/features/weather/domain/usecase/get_current_weather_usecase.dart';
+import 'package:weather/core/features/weather/presentation/cubit/weather_cubit.dart';
 import 'package:weather/core/network/network.dart';
 import 'package:weather/core/network/network_info.dart';
 import 'package:weather/core/services/location_service.dart';
@@ -13,18 +18,26 @@ Future<void> init() async {
   //! Features
   sl.registerLazySingleton(() => LocationCubit(locationService: sl()));
   // Bloc
+  sl.registerFactory(() => WeatherCubit(getCurrentWeatherUseCase: sl()));
 
   // Usecases
+  sl.registerLazySingleton(() => GetCurrentWeatherUseCase(weatherRepo: sl()));
 
   // Repository
+  sl.registerLazySingleton<WeatherRepo>(
+    () => WeatherRepoImp(networkInfo: sl(), remoteDataSource: sl()),
+  );
 
   // Datasources
+  sl.registerLazySingleton<WeatherRemoteDataSource>(
+    () => WeatherRemoteDataSourceImp(network: sl()),
+  );
 
   //! Core
   sl.registerLazySingleton<NetworkInfo>(
     () => NetworkInfoImpl(connectionChecker: sl()),
   );
-  sl.registerLazySingleton<Network>(() => Network(dio: sl()));
+  sl.registerLazySingleton<NetworkInterface>(() => Network(dio: sl()));
   sl.registerLazySingleton<LocationServiceImp>(() => LocationServiceImp());
 
   //! External
